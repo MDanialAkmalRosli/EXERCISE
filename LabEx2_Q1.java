e5c88a224f281d2ba7027613c71c510c238f6626
M. D. Akmal <61193939+MDanialAkmalRosli@users.noreply.github.com>
2020-03-05 15:19:17 +0800

    Add files via upload

---
+++ LabEx2_Q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class LabEx2_Q1 {
+   public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+
+   int num;
+   int square, cube;
+   
+   System.out.println("Number\t\tSquare\t\tCube");
+   
+   for (num = 1; num <= 10; num++) {   // process from numbers 1 to 10
+     square = (int) Math.pow(num,2);   // calculate square of the numbers
+     cube = (int) Math.pow(num,3);     // calculate cube of the numbers
+     
+     System.out.println(num + "\t\t\t\t" + square + "\t\t\t\t" + cube );  }
+   
+   }
+}
