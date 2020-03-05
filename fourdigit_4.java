7ee234908b120e35cd9c30fb2c278e740fd63645
M. D. Akmal <61193939+MDanialAkmalRosli@users.noreply.github.com>
2020-03-05 15:16:38 +0800

    Update fourdigit_4.java

--- fourdigit_4.java
+++ fourdigit_4.java
@@ -1,3 +1,5 @@
+// Question 4 //
+
 import java.util.Scanner;
 public class fourdigit_4 {
    public static void main(String[] args){    
@@ -6,15 +8,18 @@ public class fourdigit_4 {
    int sum = 0;
    String cha = "";
      
+   //asks for 4 int digits    
    System.out.println("Enter 4 digits here...");
       String num4 = sc.next();
 
+   //the code will be displayed vertically
    for (int a=0; a < num4.length(); a++) {
       System.out.println(num4.charAt(a));
       cha = String.valueOf(num4.charAt(a));
       sum = sum + Integer.parseInt(cha);
       } 
-      
+   
+   //sum of the 4 digits entered by user
    System.out.println("Sum of the 4 digits is "+ sum);
   
    }
