c36a712381809828056b6f8f48240869adff0be8
M. D. Akmal <61193939+MDanialAkmalRosli@users.noreply.github.com>
2020-03-05 15:20:39 +0800

    Update LabEx2_Q1.java

--- LabEx2_Q1.java
+++ LabEx2_Q1.java
@@ -1,3 +1,5 @@
+// Question 1 //
+
 import java.util.Scanner;
 public class LabEx2_Q1 {
    public static void main(String[] args){
@@ -6,12 +8,14 @@ public class LabEx2_Q1 {
    int num;
    int square, cube;
    
+   //display the word "Number", "Square", "Cube"
    System.out.println("Number\t\tSquare\t\tCube");
    
    for (num = 1; num <= 10; num++) {   // process from numbers 1 to 10
      square = (int) Math.pow(num,2);   // calculate square of the numbers
      cube = (int) Math.pow(num,3);     // calculate cube of the numbers
      
+    //supposed output for this code
      System.out.println(num + "\t\t\t\t" + square + "\t\t\t\t" + cube );  }
    
    }
