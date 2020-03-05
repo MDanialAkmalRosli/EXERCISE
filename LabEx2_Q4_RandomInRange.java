4a419dabc08582b0f9f4d8706f3fa4397b10ef4d
M. D. Akmal <61193939+MDanialAkmalRosli@users.noreply.github.com>
2020-03-05 15:39:16 +0800

    Update LabEx2_Q4_RandomInRange.java

--- LabEx2_Q4_RandomInRange.java
+++ LabEx2_Q4_RandomInRange.java
@@ -1,18 +1,30 @@
+// Question 4 //
+
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class LabEx2_Q4_RandomInRange {
    public static void main(String[] args){
    Scanner sc = new Scanner(System.in);
    
    int start, finish, count = 0;
    
+   // first number 
    System.out.print("Starting number?: ");
       start = sc.nextInt();
       
+   // second number   
    System.out.print("Finishing number?: ");
       finish = sc.nextInt();
+   
+   // determines whether the first number is greater or otherwise   
+   if (start >= finish){
+      count = 0; } //number will be 0 if subjected on this condition
+   else {
+      count = ThreadLocalRandom.current().nextInt(start-1, finish+1); }
       
-   for (int c = start; c <= finish; c++)  // A for loop counts steps so far
-         System.out.print(c + " "); 
+   //supposed output   
+   System.out.println("The number in range of "+ start +" and "+ finish + " is "+ count); 
    
    }
 }
